b2ba909cf4c1c4a2342ce33601aa1d47a34cb2e7
diamondrarktvo <amada.rktvo@gmail.com>
2025-02-17 16:36:29 +0300

    chore: change tag types on base api

--- front/src/services/BaseApi.ts
+++ front/src/services/BaseApi.ts
@@ -9,6 +9,6 @@ export const BaseApi = createApi({
   refetchOnReconnect: true,
   refetchOnFocus: true,
   refetchOnMountOrArgChange: true,
-  tagTypes: ["weather"],
+  tagTypes: ["Recording"],
   endpoints: () => ({}),
 });
